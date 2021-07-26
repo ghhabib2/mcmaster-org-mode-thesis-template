371967ce8a36c328117a3f7092174814936cd0d9
Habib Ghaffari Hadigheh <ghaffh1@mcmaster.ca>
2021-07-25 23:28:24 -0400

    Changing natbib referenceing style configuration

--- auto/index.el
+++ auto/index.el
@@ -4,15 +4,15 @@
    (TeX-add-to-alist 'LaTeX-provided-class-options
                      '(("report" "12pt")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
-                     '(("caption" "justification=centering") ("hyperref" "hidelinks") ("enumitem" "shortlabels") ("extdash" "shortcuts")))
+                     '(("natbib" "square" "numbers") ("caption" "justification=centering") ("hyperref" "hidelinks") ("enumitem" "shortlabels") ("extdash" "shortcuts")))
    (add-to-list 'LaTeX-verbatim-environments-local "minted")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
